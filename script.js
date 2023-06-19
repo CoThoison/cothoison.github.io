b474a2cbd3afc760bcde22388e8937aa5890121b
Corentin Thoison <thoisoc@bde.enseeiht.fr>
2023-06-19 21:42:13 +0200

    Reponsible Website

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
+const menuHamburger = document.querySelector(".menuHamburger");
+const navLinks = document.querySelector(".nav-links");
+ 
+menuHamburger.addEventListener('click',()=>{
+	navLinks.classList.toggle('mobile-menu');
+})
+
+const nav_buttons = document.querySelectorAll('ul.nav-links a');
+nav_buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    navLinks.classList.toggle('mobile-menu');
+  });
+});
+
 window.addEventListener('scroll', function() {
-    var sections = document.querySelectorAll('div[id]');
-    var currentSection = '';
+  const sections = document.querySelectorAll('div[id]');
+  let currentSection = '';
   
-    sections.forEach(function(section) {
-      var sectionTop = section.offsetTop;
-      var sectionHeight = section.clientHeight;
+  sections.forEach(function(section) {
+    let sectionTop = section.offsetTop;
+    let sectionHeight = section.clientHeight;
   
-      if (window.pageYOffset >= sectionTop - sectionHeight / 2) {
-        currentSection = section.getAttribute('id');
-      }
-    });
-  
-    var navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(function(link) {
-      link.classList.remove('current');
-      if (link.getAttribute('href') === '#' + currentSection) {
-        link.classList.add('current');
-      }
-    });
+    if (window.pageYOffset >= sectionTop - sectionHeight / 2) {
+      currentSection = section.getAttribute('id');
+    }
   });
   
+  const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(function(link) {
+    link.classList.remove('current');
+    if (link.getAttribute('href') === '#' + currentSection) {
+      link.classList.add('current');
+    }
+  });
+});
